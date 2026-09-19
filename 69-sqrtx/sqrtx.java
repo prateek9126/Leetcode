5b0ffc8173fd4efa325692ae98615f0d999b7919
Prateek Kumar <prateek2222kumar@gmail.com>
2026-09-19 16:27:32 +0530

    Time: 20 ms (11.65%) | Memory: 42.6 MB (42.92%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,18 +1,16 @@
 class Solution {
     public int mySqrt(int x) {
-
-        if(x == 0) {
+        if(x==0){
             return 0;
         }
-
+        int t=x;
+        /*if(x == 0) {
+            return 0;
+        }
         int t = x;
-
         ArrayList<Integer> arr = new ArrayList<>();
-
         int j = 2;
-
         while(x > 1 && j <= x / j) {
-
             if(x % j == 0) {
                 arr.add(j);
                 x = x / j;
@@ -21,25 +19,20 @@ class Solution {
                 j++;
             }
         }
-
-        // If something is left, it is a prime factor
         if(x > 1) {
             arr.add(x);
         }
-
         int l = 1;
-
         for(int i = 0; i < arr.size() - 1; i += 2) {
 
             if(arr.get(i).equals(arr.get(i + 1))) {
                 l = l * arr.get(i);
             }
-        }
-
-        while((long)(l + 1) * (l + 1) <= t) {
+        }*/
+        int l=1;
+        while((long)(l+1) * (l+1) <= t) {
             l++;
         }
-
         return l;
     }
 }
